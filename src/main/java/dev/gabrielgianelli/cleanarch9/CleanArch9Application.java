166b449e95ab4e99881ddc5d274c3af31a993fee
Gabriel Gianelli <gabrielgianelli@gmail.com>
2023-01-25 21:08:39 -0300

    chore: :tada: create project with spring

---
+++ src/main/java/dev/gabrielgianelli/cleanarch9/CleanArch9Application.java
@@ -0,0 +1,13 @@
+package dev.gabrielgianelli.cleanarch9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArch9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArch9Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gabrielgianelli/cleanarch9/CleanArch9ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabrielgianelli.cleanarch9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArch9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
