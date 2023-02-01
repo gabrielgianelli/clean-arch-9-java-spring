66ebb432d5ed9736b7316e40d2facaadb35095e6
Gabriel Gianelli <gabrielgianelli@gmail.com>
2023-02-01 00:03:23 -0300

    test: :test_tube: add test to make an order with discount voucher

--- src/test/java/dev/gabrielgianelli/cleanarch9/unit/OrderTests.java
+++ src/test/java/dev/gabrielgianelli/cleanarch9/unit/OrderTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import dev.gabrielgianelli.cleanarch9.domain.Order;
 import dev.gabrielgianelli.cleanarch9.domain.Product;
+import dev.gabrielgianelli.cleanarch9.domain.Voucher;
 
 public class OrderTests {
     @Test
@@ -16,6 +17,18 @@ public class OrderTests {
         order.addItem(new Product("PC Gamer", 7500.00), 1);
         var totalValue = order.totalValue();
         var expectedTotalValue = 16400.00;
+        assertEquals(totalValue, expectedTotalValue);
+    }
+
+    @Test
+    public void shouldBeAbleToMakeAnOrderWithDiscountVoucher() {
+        var voucher = new Voucher("QUINZE%", 15.00);
+        var order = new Order("892.078.830-82", voucher);
+        order.addItem(new Product("PlayStation 5", 4300.00), 1);
+        order.addItem(new Product("Nintendo Switch", 2300.00), 2);
+        order.addItem(new Product("PC Gamer", 7500.00), 1);
+        var totalValue = order.totalValue();
+        var expectedTotalValue = 13940.00;
         assertEquals(totalValue, expectedTotalValue);
     }
 }
