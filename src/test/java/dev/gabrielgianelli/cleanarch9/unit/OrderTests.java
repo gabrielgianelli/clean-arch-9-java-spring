ddff39736f52f0c8759870b7746a646ae989f580
Gabriel Gianelli <gabrielgianelli@gmail.com>
2023-02-23 00:09:17 -0300

    test: :test_tube: add test to throw exception when make an order with an expired voucher

--- src/test/java/dev/gabrielgianelli/cleanarch9/unit/OrderTests.java
+++ src/test/java/dev/gabrielgianelli/cleanarch9/unit/OrderTests.java
@@ -3,6 +3,8 @@ package dev.gabrielgianelli.cleanarch9.unit;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 import dev.gabrielgianelli.cleanarch9.domain.entity.Order;
@@ -23,7 +25,7 @@ public class OrderTests {
 
     @Test
     public void shouldBeAbleToMakeAnOrderWithDiscountVoucher() {
-        var voucher = new Voucher("QUINZE%", 15.00);
+        var voucher = new Voucher("QUINZE%", 15.00, LocalDate.of(2099,12,31));
         var order = new Order("892.078.830-82", voucher);
         order.addItem(new Product("PlayStation 5", 4300.00), 1);
         order.addItem(new Product("Nintendo Switch", 2300.00), 2);
@@ -36,5 +38,11 @@ public class OrderTests {
     @Test
     public void shouldNotBeAbleToMakeAnOrderWithInvalidCPF() {
         assertThrows(RuntimeException.class, () -> new Order("892.078.830-00", null));
+    }
+
+    @Test
+    public void shouldNotBeAbleToApplyExpiredVoucher() {
+        var voucher = new Voucher("EXPIRADO", 15.00, LocalDate.of(2022, 12, 31));
+        assertThrows(RuntimeException.class, () -> new Order("892.078.830-82", voucher));
     }
 }
